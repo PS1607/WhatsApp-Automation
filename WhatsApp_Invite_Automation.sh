e7a779c30647b79de13f37084d524f6a8d9097ee
Prakhar Sharma <77260373+PS1607@users.noreply.github.com>
2022-03-03 18:21:35 +0530

    v2.0
    Bypassed Google Chrome
    Using WhatsApp API to open the link natively.

--- WhatsApp_Invite_Automation.sh
+++ WhatsApp_Invite_Automation.sh
@@ -1,38 +1,23 @@
 #!/bin/bash  
   
-file='numbers.txt'  
-message='https://chat.whatsapp.com/F2QDW2o2zLa32gvYUsSLd1'
-links='Whatsapp_links.txt'  
-echo > $links
-i=1  
-while read line; do  
-  
-#Converting Each Number to Link  
-echo "https://wa.me/$line?text=$message"  >> $links
-i=$((i+1))  
-done < $file  
+file='numbers.txt'
+
+echo "PASTE THE WHATSAPP GROUP INVITE LINK: "  
+read message  
 
 i=1  
 while read line; do  
-  
+    
 #Opening Each Link
-open "$line"
+open "whatsapp://send?text=$message&phone=$line"
 sleep 5
 osascript <<EOD
 
   tell application "System Events"
-	key code 36
+    key code 36
   end tell
   delay 1
-  tell application "Google Chrome"
-	activate
-  end tell
-  tell application "System Events"
-	key down {command}
-	keystroke "w"
-	key up {command}
-  end tell
-EOD
-i=$((i+1))  
-done < $links  
 
+EOD
+  i=$((i+1))  
+done < $file
